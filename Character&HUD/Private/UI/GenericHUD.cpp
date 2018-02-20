dd592982220a897f97aeac7d3952ac749209d993
Mattia10 <markeus@libero.it>
2018-02-20 17:24:41 +0100

    Add HUD and Widget Class

--- Character&HUD/Private/UI/GenericHUD.cpp
+++ Character&HUD/Private/UI/GenericHUD.cpp
@@ -1,7 +1,50 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "GenericHUD.h"
+#include "UI/GenericHUD.h"
+class UUserWidget;
 
+AGenericHUD::AGenericHUD()
+{
+	GameplayHUDClass = NULL;
 
+	DeadMenuClass = NULL;
 
+	PauseMenuClass = NULL;
+}
+
+bool AGenericHUD::ShowSpecificMenu(TSubclassOf<UUserWidget> ClassToShow, bool GameOnly, bool ShowMouse)
+{
+	HideAllMenus();
+
+	if (ClassToShow)
+	{
+		//create widget then add to screen
+		UUserWidget *CreatedWidget = CreateWidget<UUserWidget>(GetOwningPlayerController(), ClassToShow);
+		CreatedWidget->AddToPlayerScreen();
+
+		if (GameOnly)
+		{
+			//set our input mode to game only-> full control of the player
+			GetOwningPlayerController()->SetInputMode(FInputModeGameOnly());
+		}
+		else
+		{
+			FInputModeUIOnly InputMode;
+
+			InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+
+			GetOwningPlayerController()->SetInputMode(InputMode);
+		}
+
+		GetOwningPlayerController()->bShowMouseCursor = ShowMouse;
+
+		return true;
+	}
+	return false;
+}
+
+void AGenericHUD::HideAllMenus()
+{
+	UWidgetLayoutLibrary::RemoveAllWidgets(this);
+}
 
--- Character&HUD/Private/UI/Widget_Gameplay.cpp
+++ Character&HUD/Private/UI/Widget_Gameplay.cpp
@@ -1,7 +1,23 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Widget_Gameplay.h"
+#include "UI/Widget_Gameplay.h"
+#include "Runtime/Core/Public/Internationalization//Text.h"
+#include "PlayerCharacter.h"
 
+float UWidget_Gameplay::GetCurrentHealth()
+{
+	if (APlayerCharacter *Player = Cast<APlayerCharacter>(GetOwningPlayerPawn()))
+	{
+		return (FMath::Clamp<float>(Player->GetCurrentHealth() / Player->GetMaxHealth(), 0.0f, 1.0f));
+	}
+	return 0.5f;
+}
 
-
-
+FText UWidget_Gameplay::GetCurrentAmmo()
+{
+	if (APlayerCharacter *Player = Cast<APlayerCharacter>(GetOwningPlayerPawn()))
+	{
+		return FText::AsNumber(Player->GetCurrentAmmo());
+	}
+	return FText::AsNumber(999);
+}
